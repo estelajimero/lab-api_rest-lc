8d5c2a35bde00040c27e9443607ce26b35c02ed3
Estela <estelajimero@users.noreply.github.com>
2022-04-10 11:14:21 +0200

    add bestSentences

--- ejercicio_2/src/pods/character/api/character.api.ts
+++ ejercicio_2/src/pods/character/api/character.api.ts
@@ -1,15 +1,14 @@
 import { Character } from './character.api-model';
-import { mockCharacterCollection } from './character.mock-data';
 import Axios from 'axios';
 
-const url = 'https://rickandmortyapi.com/api/character';
+const url = '/api/characters';
 
 export const getCharacter = async (id: number): Promise<Character> => {
-  // return mockCharacterCollection.find((h) => h.id === id);
-  const { data } = await Axios.get<Character>(`${url}/${id}`);
+  const { data } = await Axios.get(`${url}/${id}`);
   return data;
 };
 
-export const saveCharacter = async (character: Character): Promise<boolean> => {
-  return true;
+export const saveBestSentences = async (character: Character): Promise<boolean> => {
+    await Axios.put(`${url}/${character.id}`, character);
+    return true;
 };
